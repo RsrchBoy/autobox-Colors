7babfa74e46b5d2464f2602f0ae505fbdabd255c
Chris Weyl <cweyl@alumni.drew.edu>
2013-09-29 00:03:54 -0700

    initial commit

---
+++ lib/autobox/Colors.pm
@@ -0,0 +1,19 @@
+package autobox::Colors;
+
+# ABSTRACT: The great new autobox::Colors!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
